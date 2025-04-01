d02fc7035eadba3c7d99368d011bc5ea564f0899
JuanArano17 <juanarano17@gmail.com>
2025-04-02 00:53:16 +0200

    fix Nro 2304i12093i1

--- visits_farmer/streamlit_interface.py
+++ visits_farmer/streamlit_interface.py
@@ -35,12 +35,21 @@ if url_to_visit:
 
 # Automatically run Streamlit if the script is executed directly (for the .exe version)
 if __name__ == '__main__':
-    import sys
-    # If running as a bundled executable, sys.argv[0] won't end with ".py"
-    if not sys.argv[0].endswith('.py'):
-        # Running from a bundled .exe; run the app using the internal Streamlit runner
-        st._run()
+    import sys, os, tempfile
+    import streamlit.web.cli as stcli
+    # If running as a bundled executable, sys.frozen will be True.
+    if getattr(sys, 'frozen', False):
+        # Running as a bundled executable; create a temporary .py file for Streamlit to run.
+        try:
+            with open(__file__, "rb") as f:
+                code = f.read()
+        except Exception as e:
+            code = b""
+        with tempfile.NamedTemporaryFile(suffix=".py", delete=False) as tmp:
+            tmp.write(code)
+            tmp_filename = tmp.name
+        sys.argv = ["streamlit", "run", tmp_filename]
+        sys.exit(stcli.main())
     else:
-        import streamlit.web.cli as stcli
-        sys.argv = ["streamlit", "run", sys.argv[0]]
+        sys.argv = ["streamlit", "run", __file__]
         sys.exit(stcli.main())
